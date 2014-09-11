927ad319f119f44f5a806f12b71c7b5032e5fc9c
Christopher Coyle <cjcoyle2k@yahoo.com>
2014-09-11 00:44:20 -0400

    Exercise 16 Solution Added

---
+++ Chapter 1/Ex016.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex016) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; Exercise 16
+
+(define (string-last str)
+  (string-ith str (sub1 (string-length str)))) 
