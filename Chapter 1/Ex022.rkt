5da7ed9d5c83e1431fc5c46c151f77913abf86e4
Christopher Coyle <cjcoyle2k@yahoo.com>
2014-09-11 01:18:22 -0400

    Exercise 22 Solution Added

---
+++ Chapter 1/Ex022.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex022) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; Exercise 22
+
+(define (string-delete str i)
+  (string-append (substring str 0 i)
+                 (substring str (add1 i) (string-length str)))
+  
+;; The function above can NOT deal with empty strings.
